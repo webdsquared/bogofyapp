c101ec58cd89bbfdccf2bcc7e784a9b6d0d5b4f1
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-02-27 15:13:52 -0600

    update nginx configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ server "198.211.99.130", :web, :app, :db, primary: true
 
 set :application, "bogofyapp"
 set :user, "aaron"
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :user_sudo, false
 
@@ -25,11 +25,11 @@ after "deploy", "deploy:cleanup" #keep only the last 5 releases
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
