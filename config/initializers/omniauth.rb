bf2a1949de5ed879538ff0eefcb6985113dab40b
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-03-03 21:42:26 -0600

    comment out identity

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "359516364120975", "dafc58a63c67b095b21185da26a9a964"
-  provider :identity, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
+  provider :identity#, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
 end
